eb40c20244c2f8984fb3e39caba9eeafe41872dc
yosuke-tomisawa <bhav.urs@gmail.com>
2024-10-16 00:21:31 +0900

    ✨ 背景画像を追加、CSSを追加

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,20 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです
-      </p>
-    </>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです
+        </p>
+      </div>
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt="トップページMVの背景"
+        width={4000}
+        height={1200}
+      />
+    </section>
   );
 }
