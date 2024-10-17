6bd4739eb57625d4ab55888abc0037e815eba7bb
[yosuke-tomisawa] <[bhav.urs@gmail.com]>
2024-10-17 10:34:13 +0900

    ✨ sassインストール、variablesファイル作成して読み込み

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const nestConfig = {
+  sassOptions: {
+    prependData: `@import "${path.resolve(__dirname, 'app/_variables.scss')}";`,
+  },
+}
+
 export default nextConfig;
