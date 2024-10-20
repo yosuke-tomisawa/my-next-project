0ecd9473651f539202d29000a54217c51c0d532c
yosuke-tomisawa <bhav.urs@gmail.com>
2024-10-21 00:38:03 +0900

    日付やタグをコンポーネント化してソースを整理しました

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import ButtonLink from "./_components/ButtonLink";
 import styles from "./page.module.css";
 import Image from "next/image";
 
-type News = {
-  id: string;
-  title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdAt: string;
-};
+import ButtonLink from "@/_components/ButtonLink";
+import { News } from "@/_libs/microcms";
+import NewsList from "./_components/NewsList";
 
 const data: {
   contents: News[];
@@ -47,6 +40,7 @@ const data: {
 };
 
 export default function Home() {
+  const sliceData = data.contents.slice(0, 2);
   return (
     <>
       <section className={styles.top}>
@@ -66,39 +60,9 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <ul>
-          {data.contents.map((article) => (
-            <li key={article.id} className={styles.list}>
-              <div className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src={"/no-image.png"}
-                  alt={"No Image"}
-                  width={1200}
-                  height={630}
-                />
-                <dl className={styles.content}>
-                  <dt className={styles.newsItemTitle}>{article.title}</dt>
-                  <dd className={styles.meta}>
-                    <span className={styles.tag}>{article.category.name}</span>
-                    <span className={styles.date}>
-                      <Image
-                        src="/clock.svg"
-                        alt=""
-                        width={16}
-                        height={16}
-                        priority
-                      />
-                      {article.publishedAt}
-                    </span>
-                  </dd>
-                </dl>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={sliceData} />
         <div className={styles.newsLink}>
-          <ButtonLink href="/news">もっと見る</ButtonLink>
+          <ButtonLink href="/news">もっっと見る</ButtonLink>
         </div>
       </section>
     </>
