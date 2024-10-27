dbb64c903c81522ccbba88db799070aac09bc905
yosuke-tomisawa <bhav.urs@gmail.com>
2024-10-28 08:41:51 +0900

    ✨ ハンバーガーメニュー追加（実装中）

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
-
+import Menu from "../Menu";
 export default function Header() {
   return (
     <header className={styles.header}>
@@ -15,19 +15,7 @@ export default function Header() {
           priority
         />
       </Link>
-      <nav className={styles.nav}>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/news">ニュース</Link>
-          </li>
-          <li>
-            <Link href="/members">メンバー</Link>
-          </li>
-          <li>
-            <Link href="/contact">お問い合わせ</Link>
-          </li>
-        </ul>
-      </nav>
+      <Menu />
     </header>
   );
 }
