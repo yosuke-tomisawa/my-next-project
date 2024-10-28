a6e8c4df2296bab97c0115f97d3defb6539ab437
yosuke-tomisawa <bhav.urs@gmail.com>
2024-10-29 00:07:04 +0900

    ⚡️ ハンバーガメニュー実装しました

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Link from 'next/link';
 import styles from './index.module.css';
 import Image from 'next/image';
+import cx from 'classnames';
+import { useState } from 'react';
 
 export default function Menu() {
+const [isOpen, setOpen] = useState<boolean>(false);
+const open = () => setOpen(true);
+const close = () => setOpen(false);
   return (
-    <nav className={styles.nav}>
+    <div>
+    <nav className={cx(styles.nav, isOpen && styles.open)}>
       <ul className={styles.items}>
         <li>
           <Link href="/news">ニュース</Link>
@@ -16,6 +23,13 @@ export default function Menu() {
           <Link href="/contact">お問い合わせ</Link>
         </li>
       </ul>
+      <button className={cx(styles.button, styles.close)} onClick={close}>
+        <Image src="/close.svg" alt="閉じる" width={24} height={24} />
+      </button>
     </nav>
+    <button className={styles.button} onClick={open}>
+      <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+    </button>
+    </div>
   );
 }
